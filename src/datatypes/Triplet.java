55618fab85a386653217a8874bde50754cb54a00
loson <losonsky551@gmail.com>
2021-11-29 11:56:04 +0100

    moved Pair into datatypes package and implemented class holding Triplet tuple

--- src/datatypes/Triplet.java
+++ src/datatypes/Triplet.java
@@ -1,14 +1,16 @@
-package main;
+package datatypes;
 
 import java.util.Objects;
 
-public class Pair<P, Q> {
+public class Triplet<P, Q, R> {
     private P first;
     private Q second;
+    private R third;
 
-    public Pair(P first, Q second) {
+    public Triplet(P first, Q second, R third) {
         this.first = first;
         this.second = second;
+        this.third = third;
     }
 
     public P getFirst() {
@@ -19,6 +21,10 @@ public class Pair<P, Q> {
         return second;
     }
 
+    public R getThird() {
+        return third;
+    }
+
     public void setFirst(P first) {
         this.first = first;
     }
@@ -27,21 +33,20 @@ public class Pair<P, Q> {
         this.second = second;
     }
 
-    @Override
-    public String toString() {
-        return "(" + first.toString() + "," + second.toString() + ")";
+    public void setThird(R third) {
+        this.third = third;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Pair<?, ?> pair = (Pair<?, ?>) o;
-        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+        Triplet<?, ?, ?> triplet = (Triplet<?, ?, ?>) o;
+        return Objects.equals(first, triplet.first) && Objects.equals(second, triplet.second) && Objects.equals(third, triplet.third);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(first, second);
+        return Objects.hash(first, second, third);
     }
 }
