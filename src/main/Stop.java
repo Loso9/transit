def0f4141a52ebd1d0b81d11bd4a14b94846fcac
loson <losonsky551@gmail.com>
2021-11-29 10:18:03 +0100

    implementing Stop class with StopInterface

--- src/main/Stop.java
+++ src/main/Stop.java
@@ -1,4 +1,47 @@
 package main;
 
-public class Stop {
+import datatypes.*;
+import java.util.*;
+
+public class Stop implements StopInterface {
+
+    private final String stopName;
+    private Time reachableAt;
+    private LineName reachableVia;
+    private final ArrayList<LineName> lines;
+
+    public Stop(String stopName, List<LineName> lines) {
+        this.stopName = stopName;
+        this.lines = new ArrayList<>(lines);
+        //temporary init
+        reachableAt = new Time(Integer.MAX_VALUE);
+        reachableVia = null;
+    }
+
+    @Override
+    public String getStopName() {
+        return stopName;
+    }
+
+    @Override
+    public List<LineName> getLines() {
+        return lines;
+    }
+
+    @Override
+    public Pair<Time, LineName> getReachableAt() {
+        LineName reachableLine = Optional.ofNullable(reachableVia).orElseThrow(NullPointerException::new);
+        return new Pair<>(reachableAt, reachableLine);
+    }
+
+    @Override
+    public void updateReachableAt(Time time, LineName lineName) {
+        Time newTime = Optional.ofNullable(time).orElseThrow(NullPointerException::new);
+        LineName newLineName = Optional.ofNullable(lineName).orElseThrow(NullPointerException::new);
+        if (newTime.getTime() < reachableAt.getTime()) {
+            reachableAt = newTime;
+            reachableVia = newLineName;
+        }
+    }
+
 }
---
+++ src/main/StopInterface.java
@@ -0,0 +1,12 @@
+package main;
+
+import datatypes.*;
+import java.util.*;
+
+public interface StopInterface {
+    List<LineName> getLines();
+    Pair<Time, LineName> getReachableAt();
+    void updateReachableAt(Time time, LineName lineName);
+    String getStopName();
+
+}
