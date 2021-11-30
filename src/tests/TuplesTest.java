a82cf5d14d3a27cf4a6430da231b68eec383e4c2
loson <losonsky551@gmail.com>
2021-11-30 20:57:59 +0100

    changes in TuplesTest due to renaming of Quadruplet

--- src/tests/TuplesTest.java
+++ src/tests/TuplesTest.java
@@ -12,8 +12,8 @@ public class TuplesTest {
     private Pair<LineName, StopName> pair2;
     private Triplet<String, LineName, Time> triplet1;
     private Triplet<Pair<Integer, Integer>, Time, TimeDiff> triplet2;
-    private Quartet<Integer, String, List<Time>, TimeDiff> quartet1;
-    private Quartet<Integer, Triplet<Integer, Integer, String>, StopName, StopName> quartet2;
+    private Quadruplet<Integer, String, List<Time>, TimeDiff> quadruplet1;
+    private Quadruplet<Integer, Triplet<Integer, Integer, String>, StopName, StopName> quadruplet2;
 
     public void setUp() {
         pair1 = new Pair<>("pair1String", 1);
@@ -22,8 +22,8 @@ public class TuplesTest {
         triplet1 = new Triplet<>("triplet1", new LineName("triplet1"), new Time(1));
         triplet2 = new Triplet<>(new Pair<>(2, 2), new Time(2), new TimeDiff(2));
 
-        quartet1 = new Quartet<>(1, "quartet1", null, new TimeDiff(1));
-        quartet2 = new Quartet<>(2, new Triplet<>(2, 2, "quartet2"), new StopName("quartet2"), new StopName("quartet22"));
+        quadruplet1 = new Quadruplet<>(1, "quartet1", null, new TimeDiff(1));
+        quadruplet2 = new Quadruplet<>(2, new Triplet<>(2, 2, "quartet2"), new StopName("quartet2"), new StopName("quartet22"));
     }
 
     @Test
@@ -44,14 +44,14 @@ public class TuplesTest {
         assertEquals(new TimeDiff(2), triplet2.getThird());
 
         //quartets
-        assertEquals(Integer.valueOf(1), quartet1.getFirst());
-        assertEquals("quartet1", quartet1.getSecond());
-        assertNull(quartet1.getThird());
-        assertEquals(new TimeDiff(1), quartet1.getForth());
-        assertEquals(Integer.valueOf(2), quartet2.getFirst());
-        assertEquals(new Triplet<>(2, 2, "quartet2"), quartet2.getSecond());
-        assertEquals(new StopName("quartet2"), quartet2.getThird());
-        assertEquals(new StopName("quartet22"), quartet2.getForth());
+        assertEquals(Integer.valueOf(1), quadruplet1.getFirst());
+        assertEquals("quartet1", quadruplet1.getSecond());
+        assertNull(quadruplet1.getThird());
+        assertEquals(new TimeDiff(1), quadruplet1.getForth());
+        assertEquals(Integer.valueOf(2), quadruplet2.getFirst());
+        assertEquals(new Triplet<>(2, 2, "quartet2"), quadruplet2.getSecond());
+        assertEquals(new StopName("quartet2"), quadruplet2.getThird());
+        assertEquals(new StopName("quartet22"), quadruplet2.getForth());
     }
 
 
