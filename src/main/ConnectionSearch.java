f82c2bc775721a3c249a161eba49d9af0f2698e2
loson <losonsky551@gmail.com>
2021-12-01 22:44:18 +0100

    first try implementing ConnectionSearch class

--- src/main/ConnectionSearch.java
+++ src/main/ConnectionSearch.java
@@ -1,4 +1,49 @@
 package main;
 
+import datatypes.*;
+import exceptions.*;
+
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class ConnectionSearch {
+
+    private final StopsInterface stops;
+    private final LinesInterface lines;
+
+    public ConnectionSearch(StopsInterface stops, LinesInterface lines) {
+        this.stops = stops;
+        this.lines = lines;
+    }
+
+    public ConnectionData search(StopName from, StopName to, Time time) {
+        try {
+            ConnectionData connection = new ConnectionData();
+            stops.setStartingStop(from, time);
+            LinkedList<StopName> earliestStopsFrom = new LinkedList<>();
+            earliestStopsFrom.add(from);
+            while (!earliestStopsFrom.contains(to)) {
+                do {
+                    StopName stop = earliestStopsFrom.removeLast();
+                    List<LineName> linesFromStop = stops.getLines(stop);
+                    lines.updateReachable(linesFromStop, stop, time);
+                } while (!earliestStopsFrom.isEmpty());
+                Optional<Pair<StopName, Time>> connectionData = stops.earliestReachableStopAfter(time);
+                if (connectionData.isEmpty()) {
+                    return null;
+                }
+                earliestStopsFrom.add(connectionData.get().getFirst());
+            }
+            return connection;
+        }
+        catch (AlreadyLoadedStopException | AlreadyLoadedLineException | FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        finally {
+            lines.clean();
+            stops.clean();
+        }
+
+    }
 }
