f532a05f6c4791b1aff7508310c2bf7a882df24f
Loso9 <92187237+Loso9@users.noreply.github.com>
2021-12-08 14:21:43 +0100

    updating LinesInterface

--- src/main/LinesInterface.java
+++ src/main/LinesInterface.java
@@ -8,7 +8,10 @@ import java.util.*;
 
 public interface LinesInterface {
     boolean isLineLoaded(LineName lineName);
-    void updateReachable(List<LineName> lineNameList, StopName stopName, Time time) throws AlreadyLoadedLineException, FileNotFoundException, NegativeCapacityException;
-    StopName updateCapacityAndGetPrevious(LineName lineName, StopName stopName, Time time) throws AlreadyLoadedLineException, FileNotFoundException, NegativeCapacityException;
+    void updateReachable(List<LineName> lineNameList, StopName stopName, Time time) throws FileNotFoundException, NegativeCapacityException, NegativeSegmentIndexException;
+    StopName updateCapacityAndGetPreviousStop(LineName lineName, StopName stopName, Time time) throws FileNotFoundException, NegativeCapacityException, FullBusException, NegativeSegmentIndexException;
+    void loadLine(LineName lineName) throws FileNotFoundException, NegativeCapacityException, NegativeSegmentIndexException;
+    void updateLineSegments() throws FileNotFoundException;
+    Map<LineName, LineInterface> getLines();
     void clean();
 }
