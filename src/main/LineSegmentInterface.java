dadb2fbde4ccb3ee44bb663c97de5014c9e123a9
Loso9 <92187237+Loso9@users.noreply.github.com>
2021-12-08 23:15:19 +0100

    updating LineSegmentInterface
    - added extra methods for constructor parameters

--- src/main/LineSegmentInterface.java
+++ src/main/LineSegmentInterface.java
@@ -11,5 +11,14 @@ public interface LineSegmentInterface {
     void incrementCapacity(Time startTime) throws FullBusException;
     int getPassengersAt(Time time);
     Quintuplet<TimeDiff, Map<Time, Integer>, Integer, LineName, StopInterface> convertToQuintuplet();
+    void wasNotUpdated();
+    //constructor gets
+    TimeDiff getTimeToNextStop();
     Map<Time, Integer> getBuses();
+    int getCapacity();
+    LineName getLineName();
+    StopInterface getNextStop();
+    int getSegmentIndex();
+    boolean wasUpdated();
+
 }
