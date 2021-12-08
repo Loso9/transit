41863a973fc93effc0879828f6d200b82411a7ee
Loso9 <92187237+Loso9@users.noreply.github.com>
2021-12-08 14:16:53 +0100

    updating LineInterface
    - added constructor methods to interface

--- src/main/LineInterface.java
+++ src/main/LineInterface.java
@@ -1,13 +1,17 @@
 package main;
 
 import datatypes.*;
+import exceptions.FullBusException;
+
 import java.util.*;
 
 public interface LineInterface {
     void updateReachable(Time time, StopName stopName);
-    StopName updateCapacityAndGetPreviousStop(StopName stopName, Time time);
-    String getName();
+    StopName updateCapacityAndGetPreviousStop(StopName stopName, Time time) throws FullBusException;
+    Quadruplet<LineName, StopName, List<LineSegmentInterface>, List<Time>> convertToQuadruplet();
+    //constructor gets
+    LineName getLineName();
     StopName getFirstStop();
     List<LineSegmentInterface> getLineSegments();
-    Quadruplet<LineName, StopName, List<LineSegmentInterface>, List<Time>> convertToQuadruplet();
+    List<Time> getStartingTimes();
 }
