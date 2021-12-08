04d87b77278a9ba1de6f39aa429b221b2649ff66
Loso9 <92187237+Loso9@users.noreply.github.com>
2021-12-08 13:35:13 +0100

    updating MemoryLinesFactory
    - refactoring code, adding updateDatabase method, making it easier to read and comprehend

--- src/database/MemoryLinesFactory.java
+++ src/database/MemoryLinesFactory.java
@@ -1,33 +1,40 @@
 package database;
 
 import datatypes.*;
+import exceptions.NegativeSegmentIndexException;
 import main.*;
 import java.util.*;
 
 public class MemoryLinesFactory implements LinesFactoryInterface {
 
-    private final List<Quadruplet<LineName, StopName, List<LineSegmentInterface>, List<Time>>> lines;
-    private final List<Pair<LineName, List<LineSegmentInterface>>> segments;
+    private final List<LineInterface> lines;
+    private final List<LineSegmentInterface> segments;
 
     public MemoryLinesFactory(MemoryDatabase mdb) {
-        lines = mdb.readLinesAsQuadruplets();
+        lines = mdb.readLines();
         segments = mdb.readSegments();
     }
 
     @Override
     public Optional<LineInterface> createLine(LineName lineName) {
+        if (lineName == null) {
+            throw new NullPointerException();
+        }
         Integer index = checkIfContainsLine(lineName);
-        if (index != null) return Optional.of(new Line(lines.get(index).getFirst(), lines.get(index).getSecond(), lines.get(index).getThird(), lines.get(index).getForth()));
+        if (index != null) return Optional.of(new Line(lines.get(index).getLineName(), lines.get(index).getFirstStop(), lines.get(index).getLineSegments(), lines.get(index).getStartingTimes()));
         return Optional.empty();
     }
 
     @Override
-    public Optional<LineSegmentInterface> createLineSegment(LineName lineName, int segmentIndex, int capacity) {
+    public Optional<LineSegmentInterface> createLineSegment(LineName lineName, int segmentIndex) throws NegativeSegmentIndexException {
+        if (segmentIndex < 0) {
+            throw new NegativeSegmentIndexException();
+        }
         boolean exists = checkIfContainsSegment(lineName, segmentIndex);
         if (exists) {
-            for (Pair<LineName, List<LineSegmentInterface>> segment : segments) {
-                if (segment.getFirst().equals(lineName)) {
-                    return Optional.of(segment.getSecond().get(segmentIndex));
+            for (LineSegmentInterface segment : segments) {
+                if (segment.getSegmentIndex() == segmentIndex && segment.getLineName().equals(lineName)) {
+                    return Optional.of(segment);
                 }
             }
         }
@@ -35,21 +42,26 @@ public class MemoryLinesFactory implements LinesFactoryInterface {
     }
 
     @Override
-    public List<Quadruplet<LineName, StopName, List<LineSegmentInterface>, List<Time>>> getLines() {
+    public List<LineInterface> getLines() {
         return lines;
     }
 
     @Override
-    public List<Pair<LineName, List<Quintuplet<TimeDiff, Map<Time, Integer>, Integer, LineName, StopInterface>>>> getSegments() {
-        List<Quintuplet<TimeDiff, Map<Time, Integer>, Integer, LineName, StopInterface>> list;
-        List<Pair<LineName, List<Quintuplet<TimeDiff, Map<Time, Integer>, Integer, LineName, StopInterface>>>> returnList = new ArrayList<>();
-        for (Pair<LineName, List<LineSegmentInterface>> x : segments) {
-            list = new ArrayList<>();
-            for (LineSegmentInterface segment : x.getSecond()) {
-                list.add(segment.convertToQuintuplet());
+    public List<LineSegmentInterface> getSegments() {
+        return segments;
+    }
+
+    @Override
+    public void updateDatabase(List<LineSegmentInterface> lineSegments) {
+        for (LineSegmentInterface lineSegmentOuter : segments) {
+            for (LineSegmentInterface lineSegmentInner : lineSegments) {
+                //common lineName and common segmentIndex is enough to determine linesegment
+                if (lineSegmentOuter.getLineName().equals(lineSegmentInner.getLineName()) &&
+                        lineSegmentOuter.getSegmentIndex() == lineSegmentInner.getSegmentIndex()) {
+                    segments.remove(lineSegmentOuter); //remove old segment
+                    segments.add(lineSegmentInner); //add updated segment
+                }
             }
-            returnList.add(new Pair<>(x.getFirst(), list));
         }
-        return returnList;
     }
 }
