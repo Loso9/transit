8682421e67a70fdc46394516c0cded697543f6d4
Loso9 <92187237+Loso9@users.noreply.github.com>
2021-12-08 14:50:24 +0100

    updating Line
    changing implementations of methods due to bad understanding of LineSegment

--- src/main/Line.java
+++ src/main/Line.java
@@ -2,6 +2,8 @@ package main;
 
 
 import datatypes.*;
+import exceptions.FullBusException;
+
 import java.util.*;
 
 public class Line implements LineInterface {
@@ -18,28 +20,72 @@ public class Line implements LineInterface {
         if (lineSegments.isEmpty() || startingTimes.isEmpty()) {
             throw new NoSuchElementException();
         }
-        startingTimes.sort(Comparator.comparingInt(Time::getTime));
         this.startingTimes = new ArrayList<>(startingTimes);
     }
 
     @Override
     public void updateReachable(Time time, StopName stopName) {
-        //TODO
+        for (Time startingTime : startingTimes) {
+            if(stopName.equals(firstStop)) {
+                if(startingTime.getTime() >= time.getTime()) {
+                    Time nextTime = startingTime;
+                    boolean travelSegmentPossibleFirst;
+                    for (LineSegmentInterface lineSegment : lineSegments) {
+                        Triplet<Time, StopName, Boolean> nextStop = lineSegment.nextStopAndUpdateReachable(nextTime);
+                        nextTime = nextStop.getFirst();
+                        travelSegmentPossibleFirst = nextStop.getThird();
+                        if (!travelSegmentPossibleFirst) break;
+                    }
+                }
+                continue;
+            }
+
+            Time nextTime = startingTime;
+
+            for (int i = 0; i < lineSegments.size(); i++) {
+                Pair<Time, StopName> nextStop = lineSegments.get(i).nextStop(nextTime);
+                if(nextStop.getSecond().equals(stopName)) {
+                    if(nextStop.getFirst().getTime() >= time.getTime()) {
+                        Time nextTimeInnerCycle = nextStop.getFirst();
+                        boolean travelSegmentPossibleSecond;
+                        for (int j = i + 1; j < lineSegments.size(); j++) {
+                            Triplet<Time, StopName, Boolean> nextStopInnerCycle = lineSegments.get(j).nextStopAndUpdateReachable(nextTimeInnerCycle);
+                            nextTime = nextStopInnerCycle.getFirst();
+                            travelSegmentPossibleSecond = nextStopInnerCycle.getThird();
+                            if (!travelSegmentPossibleSecond) break;
+                        }
+                    }
+                    continue;
+                }
+                nextTime = nextStop.getFirst();
+            }
+        }
     }
 
     @Override
-    public StopName updateCapacityAndGetPreviousStop(StopName stopName, Time time) {
+    public StopName updateCapacityAndGetPreviousStop(StopName stopName, Time time) throws FullBusException {
         if (stopName.equals(firstStop)) {
-            throw new NoSuchElementException("You are calling method for first stop - there is no previous stop.");
+            throw new NoSuchElementException("You are calling the method for first stop - there is no previous stop.");
         }
-
-        //TODO
+        for (Time forTime: startingTimes) {
+            StopName stopToReturn = firstStop;
+            Time nextStopTime = forTime;
+            for (LineSegmentInterface lineSegment : lineSegments) {
+                Pair<Time, StopName> nextStop = lineSegment.nextStop(nextStopTime);
+                if (nextStop.getFirst().equals(time) && nextStop.getSecond().equals(stopName)) {
+                    lineSegment.incrementCapacity(time);
+                    return stopToReturn;
+                }
+                stopToReturn = nextStop.getSecond();
+                nextStopTime = nextStop.getFirst();
+            }
+        }
         return null;
     }
 
     @Override
-    public String getName() {
-        return lineName.getName();
+    public LineName getLineName() {
+        return lineName;
     }
 
     @Override
@@ -50,6 +96,11 @@ public class Line implements LineInterface {
     @Override
     public List<LineSegmentInterface> getLineSegments() {
         return lineSegments;
+    }
+
+    @Override
+    public List<Time> getStartingTimes() {
+        return startingTimes;
     }
 
     @Override
