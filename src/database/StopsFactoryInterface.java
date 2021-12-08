f13161d97727e70b6f7c8efd97c51e9272a4f343
Loso9 <92187237+Loso9@users.noreply.github.com>
2021-12-08 13:37:16 +0100

    updating StopsFactoryInterface
    - slight refactor (pairs not needed to represent StopInterface)

--- src/database/StopsFactoryInterface.java
+++ src/database/StopsFactoryInterface.java
@@ -9,15 +9,15 @@ import java.util.*;
 public interface StopsFactoryInterface {
     Optional<StopInterface> createStop(StopName stopName) throws FileNotFoundException;
     default Integer checkIfContains(StopName stopName) {
-        List<Pair<StopName, List<LineName>>> stops = getStops();
+        List<StopInterface> stops = getStops();
         int index = 0;
-        for (Pair<StopName, List<LineName>> stop : stops) {
-            if (stop.getFirst().equals(stopName)) {
+        for (StopInterface stop : stops) {
+            if (stop.getStopName().equals(stopName)) {
                 return index;
             }
             index++;
         }
         return null;
     } //returns Index or null;
-    List<Pair<StopName, List<LineName>>> getStops();
+    List<StopInterface> getStops();
 }
