b82b1707b5d6e6a3f3d6564bc1f6747fe263ae94
Loso9 <92187237+Loso9@users.noreply.github.com>
2021-12-08 23:40:44 +0100

    Update LinesFactoryInterface.java
    - changed implementation of default checkIfContainsX methods

--- src/database/LinesFactoryInterface.java
+++ src/database/LinesFactoryInterface.java
@@ -2,19 +2,21 @@ package database;
 
 import datatypes.*;
 import exceptions.NegativeCapacityException;
+import exceptions.NegativeSegmentIndexException;
 import main.*;
 
 import java.io.FileNotFoundException;
 import java.util.*;
 
 public interface LinesFactoryInterface {
-    Optional<LineInterface> createLine(LineName lineName) throws FileNotFoundException, NegativeCapacityException;
-    Optional<LineSegmentInterface> createLineSegment(LineName lineName, int segmentIndex, int capacity) throws NegativeCapacityException, FileNotFoundException; //included in linesfactory, because it shares objects
+    Optional<LineInterface> createLine(LineName lineName) throws FileNotFoundException, NegativeCapacityException, NegativeSegmentIndexException;
+    //included in linesfactory, because it shares objects
+    Optional<LineSegmentInterface> createLineSegment(LineName lineName, int segmentIndex) throws NegativeCapacityException, FileNotFoundException, NegativeSegmentIndexException;
     default Integer checkIfContainsLine(LineName lineName) {
-        List<Quadruplet<LineName, StopName, List<LineSegmentInterface>, List<Time>>> lines = getLines();
+        List<LineInterface> lines = getLines();
         int index = 0;
-        for (Quadruplet<LineName, StopName, List<LineSegmentInterface>, List<Time>> line : lines) {
-            if (line.getFirst().equals(lineName)) {
+        for (LineInterface line : lines) {
+            if (line.getLineName().equals(lineName)) {
                 return index;
             }
             index++;
@@ -23,17 +25,15 @@ public interface LinesFactoryInterface {
     } //returns Index or null;
 
     default boolean checkIfContainsSegment(LineName lineName, int segmentIndex) {
-        List<Pair<LineName, List<Quintuplet<TimeDiff, Map<Time, Integer>, Integer, LineName, StopInterface>>>> lineSegments = getSegments();
-        for (Pair<LineName, List<Quintuplet<TimeDiff, Map<Time, Integer>, Integer, LineName, StopInterface>>> segment : lineSegments) {
-            if (segment.getFirst().equals(lineName)) { //we want to look at segments which connected to lineName
-                if (segment.getSecond().get(segmentIndex) != null) { //there exists Quintuplet (segment)
-                    return true;
-                }
+        List<LineSegmentInterface> lineSegments = getSegments();
+        for (LineSegmentInterface segment : lineSegments) {
+            if (segment.getSegmentIndex() == segmentIndex && segment.getLineName().equals(lineName)) { //we want to look at segments which connected to lineName
+                return true;
             }
         }
         return false;
-
     }
-    List<Quadruplet<LineName, StopName, List<LineSegmentInterface>, List<Time>>> getLines();
-    List<Pair<LineName, List<Quintuplet<TimeDiff, Map<Time, Integer>, Integer, LineName, StopInterface>>>> getSegments();
+    List<LineInterface> getLines();
+    List<LineSegmentInterface> getSegments();
+    void updateDatabase(List<LineSegmentInterface> lineSegments) throws FileNotFoundException;
 }
